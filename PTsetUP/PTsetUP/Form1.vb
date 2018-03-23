98f44307a6b63508c85a7755c83a4a798e2cd553
ristro01 <ristro01@DEL01712>
2018-03-23 16:41:35 +0100

    Gui work

--- PTsetUP/PTsetUP/Form1.vb
+++ PTsetUP/PTsetUP/Form1.vb
@@ -1,10 +1,16 @@
 ﻿Imports System.IO
+Imports System.Reflection
 
 Public Class Form1
     Private Sub ElegantThemeButton5_Click(sender As Object, e As EventArgs) Handles btnRead.Click
         If Directory.Exists(ElegantThemeTextBox1.Text) Then
+
         Else
             lblStatus.Text = "ProfitTailer-verzeichnis existiert nicht. Bitte in den Settings anpassen..."
         End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lblStatus.Text = My.Settings.PTpath
     End Sub
 End Class
